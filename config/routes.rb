bc3216e0fe6c8c2c9dd0e4a7cb60674c97012106
Tobias Sangster <tobias@greysealcoffee.com>
2020-11-17 18:11:18 +0000

    Added sign in/out/login functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  
   root to: 'pages#home'
+
+ 
 
   # root to: "equipment#index"
   resources :equipment do
