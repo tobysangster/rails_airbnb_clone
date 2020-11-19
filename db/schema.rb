44fb439d6cbbbcdffe083579a463a1359db50ee5
silvia pintus <silviapintus.93@gmail.com>
2020-11-19 13:06:58 +0000

    location added to equipment table and form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_001802) do
+ActiveRecord::Schema.define(version: 2020_11_19_124731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_001802) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo"
+    t.string "location"
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
